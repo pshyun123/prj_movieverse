4245c9b32e8e02fe9da8b97b29ec4612b8aa7a56
hjck4433 <hjck4433@gmail.com>
2024-01-04 20:46:26 +0900

    현지 차트 관련 멤버레파지토리 수정

--- src/main/java/com/totalprj/movieverse/repository/MemberRepository.java
+++ src/main/java/com/totalprj/movieverse/repository/MemberRepository.java
@@ -20,8 +20,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndPassword(String email, String password);
     // 월별 가입자 수를 조회하는 쿼리
     @Query(nativeQuery = true, value = "SELECT m.month, COUNT(u.member_id) AS count " +
-            "FROM (SELECT DISTINCT MONTH(reg_date) AS month FROM Member WHERE reg_date >= DATE_FORMAT(CURRENT_DATE - INTERVAL 1 YEAR, '%Y-%m-01')) m " +
-            "LEFT JOIN Member u ON MONTH(u.reg_date) = m.month AND u.reg_date >= DATE_FORMAT(CURRENT_DATE - INTERVAL 1 YEAR, '%Y-%m-01') " +
+            "FROM (SELECT DISTINCT MONTH(reg_date) AS month FROM member WHERE reg_date >= DATE_FORMAT(CURRENT_DATE - INTERVAL 1 YEAR, '%Y-%m-01')) m " +
+            "LEFT JOIN member u ON MONTH(u.reg_date) = m.month AND u.reg_date >= DATE_FORMAT(CURRENT_DATE - INTERVAL 1 YEAR, '%Y-%m-01') " +
             "GROUP BY m.month ")
     List<Map<String, Object>> getMonthlySignupCount();
 
